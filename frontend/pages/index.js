7809106a87ae60dc2e3c0d3799b3e7b0675a31fe
Andreas Vieten <cryptonari@outlook.com>
2022-07-20 19:24:30 +0200

    add: implement Layout Component in App

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ProjectsDataService from '../utils/projects'
 
@@ -30,40 +28,15 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>NextPortfolio</title>
-        <meta name="description" content="Next MERN-Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to my Next MERN-Portfolio
-        </h1>
-
-        <p className={styles.description}>
-          This is a practise Project for working with Next and using a MongoDB Backend
-          {/* Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code> */}
-        </p>
-
-        <div className={styles.grid}>
-          {projects.map((project) => <ProjectCard projectData={project} key={project._id}/>)}
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <h1 className={styles.title}>
+        Welcome to my Next MERN-Portfolio
+      </h1>
+      <p className={styles.description}>
+        This is a practice Project for working with Next and using a MongoDB Backend
+      </p>
+      <div className={styles.grid}>
+        {projects.map((project) => <ProjectCard projectData={project} key={project._id}/>)}
+      </div>
     </div>
   )
 }
