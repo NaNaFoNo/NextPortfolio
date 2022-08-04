5f8cc0364e15dfbf455fb124189b0c95e213f6cb
Andreas Vieten <cryptonari@outlook.com>
2022-08-04 20:29:26 +0200

    refactor: content.js file

--- frontend/utils/content.js
+++ frontend/utils/content.js
@@ -3,41 +3,13 @@
 export const contentHome = {
   picture: "/home.jpg",
   heading: [
-    {
-      text: "Welcome,",
-      highlight: false,
-      br: true
-    },
-    {
-      text: "I'm ",
-      highlight: false,
-      br: false
-    },
-    {
-      text: "ANDREAS VIETEN.",
-      highlight: true,
-      br: true
-    },
-    {
-      text: "Graduated",
-      highlight: false,
-      br: true
-    },
-    {
-      text: "BLOCKCHAIN- /",
-      highlight: true,
-      br: true
-    },
-    {
-      text: "WEB-DEVELOPER",
-      highlight: true,
-      br: true
-    },
-    {
-      text: "@Udacity",
-      highlight: false,
-      br: false
-    },
+    { text: "Welcome,", highlight: false, br: true },
+    { text: "I'm ", highlight: false, br: false },
+    { text: "ANDREAS VIETEN.", highlight: true, br: true },
+    { text: "Graduated", highlight: false, br: true },
+    { text: "BLOCKCHAIN- /", highlight: true, br: true },
+    { text: "WEB-DEVELOPER", highlight: true, br: true },
+    { text: "@Udacity", highlight: false, br: false },
   ],
   text: 'Formerly Automotive Engineer turning Blockchain Developer. \
         Leaving my comfort zone and taking an asymmetric bet on the future. \
