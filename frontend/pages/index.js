788334706e33bc1a3d170823ff7f7d656a89fd88
Andreas Vieten <cryptonari@outlook.com>
2022-08-04 20:26:19 +0200

    add: content file added, indexPage added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,6 +8,7 @@ import {
   Text,
 } from '@chakra-ui/react';
 
+import { contentHome } from '../utils/content'
 
 export default function Home() {
   return (
@@ -18,12 +19,17 @@ export default function Home() {
       minHeight={'86vh'}
     >
       <GridItem colSpan={1} pos={'relative'} display='flex' area={'a'}>
-        <Box h='100%' w='65%' pos={'absolute'} zIndex={'-1'} bg='gray.700' clipPath={'polygon(0 0, 46% 0, 79% 100%, 0% 100%)'}>
-          
-        </Box>
+        <Box 
+          h='100%' 
+          w='65%' 
+          pos={'absolute'} 
+          zIndex={'-1'} 
+          bg='gray.700' 
+          clipPath={'polygon(0 0, 46% 0, 79% 100%, 0% 100%)'}
+        />
         <Container justifyContent='space-around' centerContent>
           <Image
-            src={'/home.jpg'}
+            src={contentHome.picture}
             objectFit={'cover'}
             w={'80%'}
             maxHeight={'80%'}
@@ -31,24 +37,23 @@ export default function Home() {
           />
         </Container>
       </GridItem>
+
       <GridItem colSpan={1} display='flex' flexDirection={'column'} justifyContent={'center'} area={'b'} m={'4rem'} >
-        
-          <Heading mb={6}>
-              Welcome,<br/>
-              I'm <span style={ {color: '#f79412'} }>ANDREAS VIETEN.</span> 
-              <br/>
-              Graduated
-              <br/>
-              <span style={ {color: '#f79412'} }>BLOCKCHAIN- /<br/>WEB-DEVELOPER</span><br/>
-              @Udacity
-          </Heading>
-          
-          <Text maxWidth={'75%'}>
-              Formerly Automotive Engineer turning Blockchain Developer. 
-              Leaving my comfort zone and taking an asymmetric bet on the future. 
-              If you want to know more about my journey into the rabbit hole, check out my site.
-          </Text>
-        
+        <Heading mb={6}>
+            {contentHome.heading.map((heading) => (
+            <>
+              { heading.highlight ? (
+                <span style={ {color: '#f79412'} }>{heading.text}</span> 
+              ) : (
+                heading.text
+              )}
+              { heading.br && (<br />)}
+            </>
+          ))}
+        </Heading>    
+        <Text maxWidth={'75%'}>
+          {contentHome.text}
+        </Text>
       </GridItem>
 
     </Grid>
---
+++ frontend/utils/content.js
@@ -0,0 +1,45 @@
+// Home Content
+
+export const contentHome = {
+  picture: "/home.jpg",
+  heading: [
+    {
+      text: "Welcome,",
+      highlight: false,
+      br: true
+    },
+    {
+      text: "I'm ",
+      highlight: false,
+      br: false
+    },
+    {
+      text: "ANDREAS VIETEN.",
+      highlight: true,
+      br: true
+    },
+    {
+      text: "Graduated",
+      highlight: false,
+      br: true
+    },
+    {
+      text: "BLOCKCHAIN- /",
+      highlight: true,
+      br: true
+    },
+    {
+      text: "WEB-DEVELOPER",
+      highlight: true,
+      br: true
+    },
+    {
+      text: "@Udacity",
+      highlight: false,
+      br: false
+    },
+  ],
+  text: 'Formerly Automotive Engineer turning Blockchain Developer. \
+        Leaving my comfort zone and taking an asymmetric bet on the future. \
+        If you want to know more about my journey into the rabbit hole, check out my site.'
+}
