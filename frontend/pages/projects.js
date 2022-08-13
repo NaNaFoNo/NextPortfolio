ff93f80d69a71617fef7778f54068d921f1b1ec4
Andreas Vieten <cryptonari@outlook.com>
2022-08-13 16:04:15 +0200

    add: getStaticProps Projects

--- frontend/pages/projects.js
+++ frontend/pages/projects.js
@@ -1,40 +1,33 @@
-import { useState, useEffect } from 'react'
-import ProjectsDataService from '../utils/projects'
 import {
   Container,
-  Wrap,
-  Text
+  Wrap
 } from '@chakra-ui/react';
 
+import ProjectsDataService from '../utils/projects'
 
 import ProjectCard from '../components/ProjectCard'
 import PageHeading from '../components/PageHeading';
 
 
-export default function Home() {
-  const [projects, setProjects] = useState([])
-
-  useEffect(() => {
-    retrieveProjects();
-  }, [])
-
-  const retrieveProjects = () => {
-    ProjectsDataService.getAllProjects()
-      .then(response => {
-        setProjects(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
+export default function Home({ projects }) {
   
-
   return (
     <Container maxW={'7xl'} p="12">
       <PageHeading page={'projects'} />
       <Wrap mt={'3rem'} spacing={'50px'} align={'center'} justify={'center'} overflow={'no'}>
-        {projects.map((project) => <ProjectCard projectData={project} key={project._id}/>)}
+        {projects?.map((project) => <ProjectCard projectData={project} key={project._id}/>)}
       </Wrap>
     </Container>
   )
+}
+
+
+export async function getStaticProps() {
+  const req = await ProjectsDataService.getAllProjects();
+
+  return {
+    props: {
+      projects: req.data,
+    }
+  }
 }
