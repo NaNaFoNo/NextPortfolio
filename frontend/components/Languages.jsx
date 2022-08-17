a89dac3cf8c23404806626ab8ffacae3f652ef34
Andreas Vieten <cryptonari@outlook.com>
2022-08-17 21:57:33 +0200

    add: first draft language percentages

--- frontend/components/Languages.jsx
+++ frontend/components/Languages.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {
   Wrap,
-  Icon
+  Badge,
+  VisuallyHidden,
+  Box
 } from '@chakra-ui/react';
 
 import TechIcon from './TechIcon';
@@ -14,11 +16,18 @@ const Languages = ({ languages }) => {
   
 
   return (
+    <>
+    <Badge width={'100%'} p={0} colorScheme={'gray'}>
+      <Badge width={'40%'} bg={'#F7DF1E'} textColor={'black'} textAlign={'center'}> 40% </Badge>
+      <Badge width={'40%'} bg={'#E34F26'} textColor={'black'} textAlign={'center'}> 40% </Badge>
+      <Badge width={'20%'} bg={'#CC6699'} textColor={'black'} textAlign={'center'}> 20% </Badge>
+    </Badge>
     <Wrap align={'center'} justify={'space-around'} direction={'row'} mt={6} h={'auto'} >
       {languageStack?.map((language, index) => (
         <TechIcon techName= {language} key={index}/>
       ))}
     </Wrap>
+    </>
   )
 }
 
