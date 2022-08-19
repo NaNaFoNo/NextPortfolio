2ca2200f736648dbfa86b4c0c288ffbddb5f56e6
Andreas Vieten <cryptonari@outlook.com>
2022-08-19 08:54:04 +0200

    add: project card language data color and order

--- frontend/components/projects/Languages.jsx
+++ frontend/components/projects/Languages.jsx
@@ -1,30 +1,52 @@
 import React from 'react'
 import {
   Wrap,
-  Badge,
-  VisuallyHidden,
   Box
 } from '@chakra-ui/react';
 
+import { svgTechIcons } from '../../utils/svgIcon'
 import TechIcon from './TechIcon';
 
 
 const Languages = ({ languages }) => {
-  const entries = Object.entries(languages)
-  const languageStack = Object.keys(languages)
-  const languageSum = Object.values(languages).reduce((a, b) => a + b)
-  
+  const sortedLanguages = Object.fromEntries(
+    Object.entries(languages).sort(([,a],[,b]) => b-a)
+  );
 
+  let LangData = []
+
+  const langValues = Object.values(sortedLanguages)
+  const langSum = langValues.reduce((a, b) => a + b)
+  const data = Object.keys(sortedLanguages).map((lang, index) => {
+    const svgObject= svgTechIcons.find((tech) => tech.name.toLowerCase() == lang.toLowerCase());
+    let obj = {}
+
+    obj.language = lang
+    obj.stat = Math.round(langValues[index] / langSum * 100)
+    obj.color = svgObject.color
+    LangData.push(obj)
+  })
+ 
   return (
     <>
-    <Badge width={'100%'} p={0} colorScheme={'gray'}>
-      <Badge width={'40%'} bg={'#F7DF1E'} textColor={'black'} textAlign={'center'}> 40% </Badge>
-      <Badge width={'40%'} bg={'#E34F26'} textColor={'black'} textAlign={'center'}> 40% </Badge>
-      <Badge width={'20%'} bg={'#CC6699'} textColor={'black'} textAlign={'center'}> 20% </Badge>
-    </Badge>
+    <Box display={'flex'} flexDir={'row'} width={'100%'} p={0} bg={'gray'} borderRadius='xl' textColor={'black'} textAlign={'center'}>
+      {LangData?.map((obj, index) => (
+        <Box 
+          key={index}
+          width={obj.stat +'%'} 
+          bg={obj.color}  
+          borderLeftRadius={index == 0 && 'xl' }
+          borderRightRadius={index == LangData.length - 1 && 'xl' }
+        >
+        
+          {  obj.stat +'%'}
+        </Box>
+      ))
+      }
+    </Box>
     <Wrap align={'center'} justify={'space-around'} direction={'row'} mt={6} h={'auto'} >
-      {languageStack?.map((language, index) => (
-        <TechIcon techName= {language} key={index}/>
+      {LangData?.map((obj, index) => (
+        <TechIcon techName= {obj.language} key={index}/>
       ))}
     </Wrap>
     </>
