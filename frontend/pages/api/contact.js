43affc4c5c9885d57ef35a791ac589cf36aead23
Andreas Vieten <cryptonari@outlook.com>
2022-11-07 14:12:59 +0100

    add: implement nodemailer in contact form

---
+++ frontend/pages/api/contact.js
@@ -0,0 +1,31 @@
+export default function (req, res) {
+  require('dotenv').config()
+  
+  let nodemailer = require('nodemailer')
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: 'vietenandreas@gmail.com',
+      pass: process.env.password,
+    },
+    secure: true,
+  })
+  const mailData = {
+    from: req.body.email,
+    to: 'vietenandreas@gmail.com',
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from:
+    ${req.body.email}</p>`
+  }
+  try {
+    transporter.sendMail(mailData)
+    console.log('res', res.json);
+    return res.status(200).end();
+  } catch (error) {
+    console.log('error', error);
+    return res.status(500).send(error);
+  }
+ 
+}
--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -18,163 +18,202 @@ import {
     VStack,
   } from '@chakra-ui/react';
 
-  import React from 'react';
-  import { BsGithub, BsLinkedin, BsPerson, BsTwitter } from 'react-icons/bs';
-  import { MdEmail, MdOutlineEmail } from 'react-icons/md';
+import React from 'react';
+import { useState, useEffect } from 'react'
+import { BsGithub, BsLinkedin, BsPerson, BsTwitter } from 'react-icons/bs';
+import { MdEmail, MdOutlineEmail } from 'react-icons/md';
+import PageHeading from '../components/PageHeading';
 
-  import PageHeading from '../components/PageHeading';
 
-  export default function ContactFormWithSocialButtons() {
-    const { hasCopied, onCopy } = useClipboard('vietenandreas+pf@gmail.com');
-  
-    return (
-      <Flex
-        bg={useColorModeValue('gray.100', 'gray.900')}
-        align="center"
-        justify="center"
-        id="contact">
-        <Box
-          borderRadius="lg"
-          m={{ base: 5, md: 16, lg: 10 }}
-          p={{ base: 5, lg: 16 }}>
-          <Box>
-            <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
-              <PageHeading page={'contact'} />
-  
-              <Stack
-                spacing={{ base: 4, md: 8, lg: 20 }}
-                direction={{ base: 'column', md: 'row' }}>
-                <Stack
-                  align="center"
-                  justify="space-around"
-                  direction={{ base: 'row', md: 'column' }}>
-                  <Tooltip
-                    label={hasCopied ? 'Email Copied!' : 'Copy Email'}
-                    closeOnClick={false}
-                    hasArrow>
-                    <IconButton
-                      aria-label="email"
-                      variant="ghost"
-                      size="lg"
-                      fontSize="3xl"
-                      icon={<MdEmail />}
-                      _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
-                      }}
-                      onClick={onCopy}
-                      isRound
-                    />
-                  </Tooltip>
-  
-                  <Link href="https://github.com/NaNaFoNo" isExternal>
-                    <IconButton
-                      aria-label="github"
-                      variant="ghost"
-                      size="lg"
-                      fontSize="3xl"
-                      icon={<BsGithub />}
-                      _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
-                      }}
-                      isRound
-                    />
-                  </Link>
-  
-                  <Link href="https://twitter.com/NaNaFo6" isExternal>
-                    <IconButton
-                      aria-label="twitter"
-                      variant="ghost"
-                      size="lg"
-                      icon={<BsTwitter size="28px" />}
-                      _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
-                      }}
-                      isRound
-                    />
-                  </Link>
-  
-                  <Link href="https://www.linkedin.com/in/andreas-vieten/" isExternal>
-                    <IconButton
-                      aria-label="linkedin"
-                      variant="ghost"
-                      size="lg"
-                      icon={<BsLinkedin size="28px" />}
-                      _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
-                      }}
-                      isRound
-                    />
-                  </Link>
-                </Stack>
-  
-                <Box
-                  bg={useColorModeValue('white', 'gray.700')}
-                  borderRadius="lg"
-                  p={8}
-                  color={useColorModeValue('gray.700', 'whiteAlpha.900')}
-                  shadow="base">
-                  <Text fontSize='2xl' color={'#f79412'} as='kbd' >
-                    {'{' }
-                    <br />   
-                  </Text>
-                  <VStack spacing={5} mt={'10px'}>
-                    <FormControl pl={'1rem'} isRequired>
-                      <FormLabel  as={'kbd'} color={'#f79412'}>name:</FormLabel>
-  
-                      <InputGroup >
-                        <InputLeftElement children={<BsPerson />} />
-                        <Input type="text" name="name" placeholder="Your Name" />
-                      </InputGroup>
-                    </FormControl>
-  
-                    <FormControl pl={'1rem'} isRequired>
-                      <FormLabel  as={'kbd'} color={'#f79412'}>email:</FormLabel>
-  
-                      <InputGroup>
-                        <InputLeftElement children={<MdOutlineEmail />} />
-                        <Input
-                          type="email"
-                          name="email"
-                          placeholder="Your Email"
-                        />
-                      </InputGroup>
-                    </FormControl>
-  
-                    <FormControl pl={'1rem'} isRequired>
-                      <FormLabel as={'kbd'} color={'#f79412'}>message:</FormLabel>
-  
-                      <Textarea
-                        name="message"
-                        placeholder="Your Message"
-                        rows={6}
-                        resize="none"
-                      />
-                      
-                    </FormControl>
-                    <Text fontSize='2xl' color={'#f79412'} as='kbd' alignSelf={'start'}>
-                        {'}' }
-                    </Text>
-  
-                    <Button
-                      colorScheme="blue"
-                      bg="blue.400"
-                      color="white"
-                      _hover={{
-                        bg: 'blue.500',
-                      }}
-                      isfullwidth="true">
-                      Send Message
-                    </Button>
-                  </VStack>
-                </Box>
-              </Stack>
-            </VStack>
-          </Box>
-        </Box>
-      </Flex>
-    );
+
+
+export default function ContactFormWithSocialButtons() {
+  const { hasCopied, onCopy } = useClipboard('vietenandreas+pf@gmail.com');
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleSubmit = (e) => { 
+    e.preventDefault()
+    console.log('Sending')
+    let data = {
+        name,
+        email,
+        message
+    }
+    fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then((res) => {
+      console.log('Response received')
+      if (res.status === 200) {
+        console.log('Response succeeded!')
+        setSubmitted(true)
+        setName('')
+        setEmail('')
+        setMessage('')
+      }
+    })
   }
+
+  return (
+    <Flex
+      bg={useColorModeValue('gray.100', 'gray.900')}
+      align="center"
+      justify="center"
+      id="contact">
+      <Box
+        borderRadius="lg"
+        m={{ base: 5, md: 16, lg: 10 }}
+        p={{ base: 5, lg: 16 }}>
+        <Box>
+          <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
+            <PageHeading page={'contact'} />
+
+            <Stack
+              spacing={{ base: 4, md: 8, lg: 20 }}
+              direction={{ base: 'column', md: 'row' }}>
+              <Stack
+                align="center"
+                justify="space-around"
+                direction={{ base: 'row', md: 'column' }}>
+                <Tooltip
+                  label={hasCopied ? 'Email Copied!' : 'Copy Email'}
+                  closeOnClick={false}
+                  hasArrow>
+                  <IconButton
+                    aria-label="email"
+                    variant="ghost"
+                    size="lg"
+                    fontSize="3xl"
+                    icon={<MdEmail />}
+                    _hover={{
+                      bg: 'blue.500',
+                      color: useColorModeValue('white', 'gray.700'),
+                    }}
+                    onClick={onCopy}
+                    isRound
+                  />
+                </Tooltip>
+
+                <Link href="https://github.com/NaNaFoNo" isExternal>
+                  <IconButton
+                    aria-label="github"
+                    variant="ghost"
+                    size="lg"
+                    fontSize="3xl"
+                    icon={<BsGithub />}
+                    _hover={{
+                      bg: 'blue.500',
+                      color: useColorModeValue('white', 'gray.700'),
+                    }}
+                    isRound
+                  />
+                </Link>
+
+                <Link href="https://twitter.com/NaNaFo6" isExternal>
+                  <IconButton
+                    aria-label="twitter"
+                    variant="ghost"
+                    size="lg"
+                    icon={<BsTwitter size="28px" />}
+                    _hover={{
+                      bg: 'blue.500',
+                      color: useColorModeValue('white', 'gray.700'),
+                    }}
+                    isRound
+                  />
+                </Link>
+
+                <Link href="https://www.linkedin.com/in/andreas-vieten/" isExternal>
+                  <IconButton
+                    aria-label="linkedin"
+                    variant="ghost"
+                    size="lg"
+                    icon={<BsLinkedin size="28px" />}
+                    _hover={{
+                      bg: 'blue.500',
+                      color: useColorModeValue('white', 'gray.700'),
+                    }}
+                    isRound
+                  />
+                </Link>
+              </Stack>
+
+              <Box
+                bg={useColorModeValue('white', 'gray.700')}
+                borderRadius="lg"
+                p={8}
+                color={useColorModeValue('gray.700', 'whiteAlpha.900')}
+                shadow="base">
+                <Text fontSize='2xl' color={'#f79412'} as='kbd' >
+                  {'{' }
+                  <br />   
+                </Text>
+                <VStack spacing={5} mt={'10px'}>
+                  <FormControl pl={'1rem'} isRequired>
+                    <FormLabel  as={'kbd'} color={'#f79412'}>name:</FormLabel>
+
+                    <InputGroup >
+                      <InputLeftElement children={<BsPerson />} />
+                      <Input type="text" name="name" value={name} placeholder="Your Name" onChange={(e)=>{setName(e.target.value)}}/>
+                    </InputGroup>
+                  </FormControl>
+
+                  <FormControl pl={'1rem'} isRequired>
+                    <FormLabel  as={'kbd'} color={'#f79412'}>email:</FormLabel>
+
+                    <InputGroup>
+                      <InputLeftElement children={<MdOutlineEmail />} />
+                      <Input
+                        type="email"
+                        name="email"
+                        value={email}
+                        placeholder="Your Email"
+                        onChange={(e)=>{setEmail(e.target.value)}}
+                      />
+                    </InputGroup>
+                  </FormControl>
+
+                  <FormControl pl={'1rem'} isRequired>
+                    <FormLabel as={'kbd'} color={'#f79412'}>message:</FormLabel>
+
+                    <Textarea
+                      name="message"
+                      placeholder="Your Message"
+                      rows={6}
+                      resize="none"
+                      value={message}
+                      onChange={(e)=>{setMessage(e.target.value)}}
+                    />
+                    
+                  </FormControl>
+                  <Text fontSize='2xl' color={'#f79412'} as='kbd' alignSelf={'start'}>
+                      {'}' }
+                  </Text>
+
+                  <Button
+                    colorScheme="blue"
+                    bg="blue.400"
+                    color="white"
+                    _hover={{
+                      bg: 'blue.500',
+                    }}
+                    isfullwidth="true"
+                    onClick={(e)=>{handleSubmit(e)}}>
+                    Send Message
+                  </Button>
+                </VStack>
+              </Box>
+            </Stack>
+          </VStack>
+        </Box>
+      </Box>
+    </Flex>
+  );
+}
